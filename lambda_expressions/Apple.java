fbe60a1113e218c17bba709729758d590869af8c
Atish Anant <atishanant10@gmail.com>
2020-09-17 10:57:11 +0530

    initial commmit to initialize git

---
+++ lambda_expressions/Apple.java
@@ -0,0 +1,20 @@
+public class Apple {
+    String color;
+    Double weight;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+}
